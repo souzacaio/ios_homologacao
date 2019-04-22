a14a43954432fb0ab7144d3155979bc9c5633743
Caio <caio.souza@live.com>
2019-04-22 14:40:09 -0300

    sqlite

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -439,7 +439,7 @@ module.exports.metadata =
     "cordova-plugin-dialogs": "1.2.1",
     "cordova-plugin-file": "6.0.1",
     "cordova-plugin-file-transfer": "1.5.1",
-    "cordova-sqlite-storage": "3.2.1-dev"
+    "cordova-sqlite-storage": "3.2.0"
 }
 // BOTTOM OF METADATA
 });
